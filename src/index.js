919ceb5c6e800d1fc5eee52315473ad7dc123940
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-11-08 00:13:37 +0630

    Get the hamburger menu working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { projectTracker, Project, ToDo } from "./todos/todo.js";
 import toDoData from "./todos/todo-data.json";
+import "./style.css";
 
 let defaultProject = new Project();
 let workProject = new Project("work");
@@ -14,6 +15,7 @@ projectTracker.addProject(workProject);
 
 let allProjects = projectTracker.getAllProjects();
 let projectList = document.createElement("ul");
+projectList.classList.add("projectMenu");
 
 allProjects.forEach(eachProject => {
     let projectItem = document.createElement("li");
@@ -21,4 +23,20 @@ allProjects.forEach(eachProject => {
     projectList.appendChild(projectItem);
 });
 
+let hamburgerBtn = document.getElementById("hamburger");
+let openIcon = document.getElementById("openIcon");
+let closeIcon = document.getElementById("closeIcon");
+
+hamburgerBtn.addEventListener("click", e => {
+    if(projectList.classList.contains("showProjectMenu")) {
+        openIcon.style.display = "inline";
+        closeIcon.style.display = "none";
+    } else {
+        openIcon.style.display = "none";
+        closeIcon.style.display = "inline";
+    }
+    projectList.classList.toggle("showProjectMenu");
+})
+
+document.body.appendChild(projectList);
 
